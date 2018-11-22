4ecb48c11f5de82c307840d6176f18d8105f44f1
Eduardo Rubio <eduardo.rubio.jr85@gmail.com>
2018-11-22 11:57:50 -0500

    Test to make sure repo is linked correctly

---
+++ crazyHouse/crazyHouse/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  crazyHouse
+//
+//  Created by Eduardo Rubio on 11/22/18.
+//  Copyright © 2018 Eduardo Rubio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
