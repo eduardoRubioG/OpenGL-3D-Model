773c54378abfad2817d7edc05e40ea183415f29d
Eduardo Rubio <eduardo.rubio.jr85@gmail.com>
2018-11-23 14:24:34 -0500

    Upload New File

---
+++ include.h
@@ -0,0 +1,33 @@
+//
+//  include.hpp
+//  crazyHouse
+//
+//  Created by Eduardo Rubio on 11/22/18.
+//  Copyright © 2018 Eduardo Rubio. All rights reserved.
+//
+
+#ifndef include_hpp
+#define include_hpp
+
+//Linux Headers
+//#ifdef LINUX
+//#include <GL/glut.h>
+//#endif
+
+//Mac Headers
+//#ifdef OSX
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+//#endif
+
+#include <stdio.h>
+#include "globals.h"
+#include "axes.cpp"
+#include "house.cpp"
+#include "animation.cpp"
+#include "init.cpp"
+#include "input.cpp"
+
+
+#endif /* include_hpp */
